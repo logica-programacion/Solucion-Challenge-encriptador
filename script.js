53725a2eab83c288ee2729aa140d6ba4ecfce6dd
Jeanmarie Quijada <92184087+JeanmarieAluraLatam@users.noreply.github.com>
2023-01-03 23:30:48 -0300

    funcion copiar y validar texto

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
+const copia = document.querySelector(".copiar");
+copia.style.display = "none"
+
+function validarTexto(){
+    let textoEscrito = document.querySelector(".text-area").value;
+    let validador = textoEscrito.match(/^[a-z]*$/);
+
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
+        location.reload();
+        return true;
+    }
+}
+
+validarTexto()
 
 
+function btnEncriptar(){
+    if(!validarTexto()) {
+        const textoEncriptado = encriptar(textArea.value)
+        mensaje.value = textoEncriptado
+        mensaje.style.backgroundImage = "none"
+        textArea.value = "";
+        copia.style.display = "block"
+    
+    }
+}
+
+//Laves de encriptacion
 // `La letra "e" es convertida para "enter"`
 // `La letra "i" es convertida para "imes"`
 // `La letra "a" es convertida para "ai"`
 // `La letra "o" es convertida para "ober"`
 // `La letra "u" es convertida para "ufat"`
-
-
-function btnEncriptar(){
-    const textoEncriptado = encriptar(textArea.value)
-    mensaje.value = textoEncriptado
-    textArea.value = "";
-    mensaje.style.backgroundImage = "none"
-}
 
 
 function encriptar(stringEncriptada){
@@ -54,3 +73,14 @@ function desencriptar(stringDesencriptada){
     }
     return stringDesencriptada
 }
+
+
+function copiar(){
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+    alert("Texto Copiado")
+}
+
+
+
